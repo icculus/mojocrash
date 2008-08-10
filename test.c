eae0c00cb89d7bcfc6276103317c82bc446bfd18
Ryan C. Gordon <icculus@icculus.org>
2008-08-10 22:20:18 +0200

    Added test program.

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "mojocrash.h"
+
+void f4() { printf("crashing!\n"); fflush(stdout); *((int *)0) = 0; }
+void f3() { f4(); }
+void f2() { f3(); }
+void f1() { f2(); }
+   
+int
+main()
+{
+    if (!MOJOCRASH_install("myapp", "1.0", "http://icculus.org/", NULL))
+        printf("failed to install!\n");
+    else
+        f1();
+    return 0;
+}
+
