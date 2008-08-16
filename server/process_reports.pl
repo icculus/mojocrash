7a4be2fe5b0acce1f1d1e9fe6577741a7c94a939
Ryan C. Gordon <icculus@icculus.org>
2008-08-16 19:32:29 +0200

    Bunch More Work.

--- server/process_reports.pl
+++ server/process_reports.pl
@@ -9,12 +9,14 @@ my $dbhost = 'localhost';
 my $dbuser = 'root';
 my $dbpass = undef;
 my $dbpassfile = 'dbpass.txt';
+my $dbname = 'mojocrash';
 
 my %apps;
 my %appvers;
 my %plats;
 my %platvers;
 my %cpus;
+my %boguses;
 
 # !!! FIXME: grep for 'die' and remove it. This needs to stay up in face of
 # !!! FIXME:  temporary failures, and not take down other threads, too!
@@ -63,8 +65,23 @@ sub add_static_id {
 }
 
 
+sub poke_bugzilla {
+    my ($newbug, $report, $trackerid) = @_;
+    if ($newbug) {
+        # !!! FIXME: post to url, get $trackerid
+        print "new bug to bugzilla\n";
+        $trackerid = int(rand());
+    } else {
+        # !!! FIXME: post to other url
+        print "update bug in bugzilla\n";
+    }
+    
+    return $trackerid;
+}
+
+
 sub poke_bugtracker {
-    my ($guid, $stack, $cmdargsref, $etcsref) = @_;
+    my ($id, $guid, $stack, $cmdargsref, $etcsref) = @_;
     my $processed_text = '';
 
     my $link = get_database_link();
@@ -73,10 +90,9 @@ sub poke_bugtracker {
     $sth->execute() or die "can't execute the query: $sth->errstr";
     my @row = $sth->fetchrow_array();
     my $newbug = not @row;
-    my $id = $newbug ? 0 : $row[0];
+    my $postid = $newbug ? 0 : $row[0];
     my $trackerid = $newbug ? 0 : $row[1];
     $sth->finish();
-
 
     my $report;
     if ($newbug) {
@@ -85,7 +101,9 @@ sub poke_bugtracker {
         $report = "Another report with same callstack.\n\n";
     }
 
-    foreach (keys $$cmdargsref) {
+    $report .= "MojoCrash report #$id, post #$postid, guid $guid\n";
+
+    foreach (keys $cmdargsref) {
         $report .= $_ . ': ' . $$cmdargsref[$_] . "\n";
     }
 
@@ -94,6 +112,11 @@ sub poke_bugtracker {
     }
 
     $report .= "\n\n";
+
+    # !!! FIXME: handle stuff other than Bugzilla.
+    $trackerid = poke_bugzilla($newbug, $report, $trackerid);
+        
+    return ($report, $trackerid);
 }
 
 
@@ -102,6 +125,7 @@ sub handle_unprocessed_report {
     my $text = shift;
 
     my $line = 0;
+    my $bogus = undef;
     my $appname = undef;
     my $appver = undef;
     my $platname = undef;
@@ -195,12 +219,14 @@ sub handle_unprocessed_report {
                 unshift @callstack, $args;  # can't push; it's reverse order.
             }
         } elsif ($cmd eq 'ETC_KEY') {
+            $multiple_okay = 1;
             if ($etcs{$args}) {
                 $bogus = 'duplicate ETC_KEY';
             } else {
                 $etckey = $args;
             }
         } elsif ($cmd eq 'ETC_VALUE') {
+            $multiple_okay = 1;
             if ($lastcmd ne 'ETC_KEY') {
                 $bogus = 'ETC_VALUE not following ETC_KEY';
             } else {
@@ -219,7 +245,7 @@ sub handle_unprocessed_report {
 
         last if (defined $bogus);  # something failed, so stop.
 
-        $cmdargs{$cmd} = $args;
+        $cmdargs{$cmd} = $args if (!multiple_okay);
         $lastcmd = $cmd;
         $seen{$cmd} = 1;
     }
@@ -243,13 +269,14 @@ sub handle_unprocessed_report {
 
     my $guid = undef;
     my $processed_text = undef;
+    my $postid = undef;
     if (not defined $bogus) {
         # okay, we're parsed now. Now it's time to process the information.
         #  first, we need to convert the stack into source files and line
         #  numbers.
         my $processed_callstack = convert_callstack(\@callstack, \%objs);
         $guid = sha1($appname . $appver . $processed_callstack);
-        $processed_text = poke_bugtracker($guid, $processed_callstack, \%cmdargs, \%etcs);
+        ($processed_text, $postid) = poke_bugtracker($guid, $processed_callstack, \%cmdargs, \%etcs);
     }
 
     my $link = get_database_link();
@@ -259,7 +286,7 @@ sub handle_unprocessed_report {
         # In theory, there are a finite number of these, all listed in the
         #  code, but it's easier to have this programatically update as things
         #  change.
-        $bogusid = $boguses{$bogus};
+        my $bogusid = $boguses{$bogus};
         if (not defined $bogusid) {
             $bogusid = add_static_id('bogus_reasons', 'reason', $bogus);
             $boguses{$bogus} = $bogusid;
@@ -267,9 +294,17 @@ sub handle_unprocessed_report {
         $sql = "update reports set status=-1, bogus_line=$line," .
                " bogus_reason=$bogusid where id=$id";
     } else {
-        $sql = "update reports set status=1, s;lkjfs;ldkfj where id=$id";
+        $processed_text = $link->quote($processed_text);
+        $sql = "update reports set status=1, bugtracker_entry=$postid," .
+               " processed_text=$processed_text app_id=$appname," .
+               " platform_id=$platname, platform_version_id=$platver," .
+               " cpuarch_id=$cpuname, crashsignal=$signal," .
+               " crashtime=$crashtime, uptime=$uptime where id=$id";
     }
+
+    $link->do($sql);
 }
+
 
 sub load_static_id_hash {
     my ($hashref, $table, $field) = @_;
@@ -290,6 +325,7 @@ sub load_static_ids {
     load_static_id_hash(\%plats, 'platforms', 'name');
     load_static_id_hash(\%platvers, 'platformversions', 'version');
     load_static_id_hash(\%cpus, 'cpuarchs', 'name');
+    load_static_id_hash(\%boguses, 'bogus_reasons', 'reason');
 }
 
 sub run_unprocessed {
--- server/schema.sql
+++ server/schema.sql
@@ -22,6 +22,11 @@ create table platforms (
     primary key (id)
 ) character set utf8;
 
+insert into platforms (name) values ("linux");
+insert into platforms (name) values ("macosx");
+insert into platforms (name) values ("windows");
+insert into platforms (name) values ("beos");
+
 create table platformversions (
     id int unsigned not null auto_increment,
     version varchar(64) not null,
@@ -33,6 +38,11 @@ create table cpuarchs (
     name varchar(64) not null,
     primary key (id)
 ) character set utf8;
+
+insert into cpuarchs (name) values ("x86");
+insert into cpuarchs (name) values ("x86-64");
+insert into cpuarchs (name) values ("powerpc");
+insert into cpuarchs (name) values ("powerpc64");
 
 create table bogus_reasons (
     id int unsigned not null auto_increment,
@@ -58,7 +68,7 @@ create table reports (
     -- everything below here isn't filled in until report is processed.
     bogus_line int unsigned,
     bogus_reason_id int unsigned,
-    tracker_entry int unsigned,
+    bugtracker_post int unsigned,
     processed_text mediumtext,
     app_id int unsigned,
     platform_id int unsigned,
